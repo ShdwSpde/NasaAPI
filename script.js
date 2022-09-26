79be965fce6c87234d8314af76b1fa15a4aa24c8
Mike Spade <mikespadeny@gmail.com>
2022-09-26 14:30:42 +0000

    overflow-y added in css
    ** still needs video/img toggle optimization

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 document.querySelector('button').addEventListener('click',getFetch)
 
 function getFetch(){
-  
-  const url = 'https://api.nasa.gov/planetary/apod?api_key=vIn5A36tyJL0QLmQG6ahSCP89ei7GYOtTa1XGD3U'
+  const choice = document.querySelector('input').value
+  const url = `https://api.nasa.gov/planetary/apod?api_key=vIn5A36tyJL0QLmQG6ahSCP89ei7GYOtTa1XGD3U&date=${choice}`
   
   fetch(url) 
     .then(res => res.json())
     .then(data => {
          console.log(data)
-      document.querySelector('img').src = data.hdurl
+      if (data.media_type === 'image'){
+        document.querySelector('img').src = data.hdurl
+      } else if (data.media_type === 'video'){
+        document.querySelector('iframe').src = data.url
+      }
       document.querySelector('h2').innerText = data.title
       document.querySelector('p').innerText = data.explanation
     })
